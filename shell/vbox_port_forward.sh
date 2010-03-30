95c09280671c5b1c9469552d70bbeacd0c1c1892
Tiago Miguel Cavaco Rodrigues <tmcrodrigues@gmail.com>
2010-03-30 12:02:28 +0100

    changed virtualbox port forwarding script to accept cli params

--- shell/vbox_port_forward.sh
+++ shell/vbox_port_forward.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 
-echo "your vm name ?"; read vmname
+if [ $# -ge 4 ] ; then
+    vmname=$1
+    service=$2
+    hostport=$3
+    guestport=$4
+    if [ $# -eq 5 ] ; then
+        protocol=$5
+    else
+        protocol=""
+    fi
+else
+    echo "your vm name ?"; read vmname
+    echo "Service name ? (ssh, www, etc)"; read service
+    echo "host port ?"; read hostport
+    echo "guest port ?"; read guestport
+    echo "Protocol ? (default: TCP)"; read protocol
+fi
 
-echo "Service name ? (ssh, www, etc)"; read service
-
-echo "host port ?"; read hostport
-
-echo "guest port ?"; read guestport
-
-echo "Protocol ? (default: TCP)"; read protocol
-
-if [ $protocol == "" ] ; then
+if [ "$protocol" = "" ] ; then
     protocol="TCP"
 fi
 
 VBoxManage setextradata "$vmname" \
-    "VBoxInternal/Devices/e1000/0/LUN#0/Config/$service/Protocol" TCP
+    "VBoxInternal/Devices/e1000/0/LUN#0/Config/$service/Protocol" $protocol
 VBoxManage setextradata "$vmname" \
     "VBoxInternal/Devices/e1000/0/LUN#0/Config/$service/HostPort" $hostport
 VBoxManage setextradata "$vmname" \
